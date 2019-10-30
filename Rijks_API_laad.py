5aa38792e43e482c382cc5f915b88bdda5f101da
Douwe de Jong <douwe.nicolaas@gmail.com>
2019-10-30 21:44:49 +0100

    2 functies die een tuple met gegevens opleveren van een zoek opdracht en de link naar de afbeelding

--- Rijks_API_laad.py
+++ Rijks_API_laad.py
@@ -1,25 +1,52 @@
 import http.client, urllib.parse, json
 
-# https://www.rijksmuseum.nl/api/nl/collection?key=UYzK8jDV&format=json&type=schilderij&f.normalized32Colors.hex=%20%23367614
+
+# opvragen
+
+def opvragenKunststuk(invoer):
+    params = urllib.parse.urlencode({
+        'p': '0',
+        'ps': '1',
+        'q': str(invoer)
+    })
+
+    conn = http.client.HTTPSConnection('www.rijksmuseum.nl')
+    conn.request("GET", "/api/nl/collection?key=UYzK8jDV&format=json&" + params)
+
+    response = conn.getresponse()
+    responsetext = response.read()
+    data = json.loads(responsetext)
+    schilderij = data["artObjects"]
+
+    with open("search_result.json", "w") as json_file:
+        json.dump(schilderij, json_file, indent=4)
+
+    for data in schilderij:
+        # return data["objectNumber"]
+        return data["principalOrFirstMaker"], data["title"], data["objectNumber"]
 
 
-params = urllib.parse.urlencode({
-    'p': '0',
-    'ps': '100',
-    'q': input("naam :")
-})
-conn = http.client.HTTPSConnection('www.rijksmuseum.nl')
-conn.request("GET", "/api/nl/collection?key=UYzK8jDV&format=json&" + params)
+def imgKunstwerk(invoer):
+    params = urllib.parse.urlencode({
+        'p': '0',
+        'ps': '1',
+        'q': str(invoer)
+    })
 
-response = conn.getresponse()
-responsetext = response.read()
-data = json.loads(responsetext)
-schilderij = data["artObjects"]
+    conn = http.client.HTTPSConnection('www.rijksmuseum.nl')
+    conn.request("GET", "/api/nl/collection?key=UYzK8jDV&format=json&" + params)
 
-print("count = {}".format(data["count"]))
+    response = conn.getresponse()
+    responsetext = response.read()
+    data = json.loads(responsetext)
+    schilderij = data["artObjects"]
+    for data in schilderij:
+        return data["webImage"]["url"]
 
-for data in schilderij:
-    print("{:40} \t {:60} \t {:15}".format(data["principalOrFirstMaker"], data["title"], data["objectNumber"]))
 
-with open("search_result.json", "w") as json_file:
-    json.dump(schilderij, json_file, indent=4)
+# invoer
+
+text = input("titel van het kunstwerk: ")
+
+print(opvragenKunststuk(text))
+print(imgKunstwerk(text))
