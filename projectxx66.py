8d62f52510d97a0f829534b429df8143e5552651
anoukvandenham17 <56221522+anoukvandenham17@users.noreply.github.com>
2019-11-01 13:00:58 +0100

    Add files via upload

--- projectxx66.py
+++ projectxx66.py
@@ -20,15 +20,19 @@ ticketscherm = Frame(root)
 for frame in(main, inloghouder, inloggebruiker, gebruiker, houder, nietgeleend, geleend, bezoekers, controle, selectie, ticketscherm):
     frame.grid(row=0, column=0, sticky='news')
 
-#mainscreen
 
-Label(main, text='Maak een keuze:').pack(padx=275, pady=15)
-Button(main, text='Galeriehouder', command=lambda: raise_frame(inloghouder)).pack(pady=15)
-Button(main, text='Gebruiker', command=lambda: raise_frame(inloggebruiker)).pack()
+#mainscreen
+Label(main, text='').grid(row=0, column=1, pady=20)
+Label(main, text='Maak een keuze:').grid(row=1, column=3, padx=125, pady=10)
+Label(main, text='').grid(row=2, column=1)
+Button(main, text='Galeriehouder', command=lambda: raise_frame(inloghouder)).grid(row=3, column=3)
+Label(main, text='').grid(row=4, column=2)
+Button(main, text='Gebruiker', command=lambda: raise_frame(inloggebruiker)).grid(row=5, column=3)
 
 
 #inloggen gebruiker
-Label(inloggebruiker, text= 'Voer uw gegevens in:').grid(row=1, column=7)
+Label(inloggebruiker, text='').grid(row=0, column=1, pady=20)
+Label(inloggebruiker, text= 'Voer uw gegevens in:').grid(row=1, column=7, padx=50, pady=10)
 Label(inloggebruiker, text= '').grid(row=2,column=3)
 Label(inloggebruiker, text= 'Naam').grid(row=3,column=5)
 Label(inloggebruiker, text= 'Email adres').grid(row=4,column=5)
@@ -45,7 +49,8 @@ Label(inloggebruiker, text= '').grid(row=7,column=3)
 Button(inloggebruiker, text= 'Terug', command= lambda: raise_frame(main)).grid(row=8, column=7)
 
 #inloggen galeriehouder
-Label(inloghouder, text= 'Voer uw gegevens in:').grid(row=1, column=7)
+Label(inloghouder, text='').grid(row=0, column=1, pady=20)
+Label(inloghouder, text= 'Voer uw gegevens in:').grid(row=1, column=7, padx=50, pady=10)
 Label(inloghouder, text= '').grid(row=2,column=3)
 Label(inloghouder, text= 'Naam').grid(row=3,column=5)
 Label(inloghouder, text= 'Wachtwoord').grid(row=4,column=5)
@@ -62,34 +67,41 @@ Label(inloghouder, text= '').grid(row=7,column=3)
 Button(inloghouder, text= 'Terug', command= lambda: raise_frame(main)).grid(row=8, column=7)
 
 #gebruiker
+Label(gebruiker, text='').grid(row=0, column=1, pady=20)
+Label(gebruiker, text= 'Maak een keuze:').grid(row=1, column=3, padx=125, pady=10)
+Label(gebruiker, text='').grid(row =2, column=2)
+Button(gebruiker, text= 'Kunststuk selectie', command= lambda: raise_frame(selectie)).grid(row=3, column=3)
+Label(gebruiker, text='').grid(row=4, column=1)
+Button(gebruiker, text= 'Ticket scherm', command= lambda: raise_frame(ticketscherm)).grid(row=5, column=3)
+Label(gebruiker, text='').grid(row=6, column=3, pady=10)
 
-Label(gebruiker, text= 'Maak een keuze:').pack(pady= 15, padx=275)
-Button(gebruiker, text= 'Kunststuk selectie', command= lambda: raise_frame(selectie)).pack(pady=15)
-Button(gebruiker, text= 'Ticket scherm', command= lambda: raise_frame(ticketscherm)).pack()
-
-Button(gebruiker, text= 'Terug', command= lambda: raise_frame(main)).pack(pady=30)
+Button(gebruiker, text= 'Terug', command= lambda: raise_frame(main)).grid(row=7, column=3)
 
 #selectie
 
-Button(selectie, text= 'Terug', command= lambda: raise_frame(gebruiker)).pack()
+Button(selectie, text= 'Terug', command= lambda: raise_frame(gebruiker)).grid()
 
 #ticketscherm
 
-Button(ticketscherm, text= 'Terug', command= lambda: raise_frame(gebruiker)).pack()
+Button(ticketscherm, text= 'Terug', command= lambda: raise_frame(gebruiker)).grid()
 
 #galeriehouder
-
-Label(houder, text= 'Maak een keuze:').pack(pady= 15, padx=275)
-Button(houder, text= 'Overzicht niet geleende kunststukken', command= lambda: raise_frame(nietgeleend)).pack(pady=10)
-Button(houder, text= 'Overzicht geleende kunststukken', command= lambda: raise_frame(geleend)).pack(pady=10)
-Button(houder, text= 'Overzicht van bezoekers',command= lambda: raise_frame(bezoekers)).pack(pady=10)
-Button(houder, text= 'Ticketnummer controle', command= lambda: raise_frame(controle)).pack(pady=10)
-
-Button(houder, text= 'Terug', command= lambda: raise_frame(main)).pack(pady=30)
+Label(houder, text='').grid(row=0, column=1)
+Label(houder, text= 'Maak een keuze:').grid(row=1, column=3, padx=125, pady=10)
+Label(houder, text='').grid(row=2, column=2)
+Button(houder, text= 'Overzicht niet geleende kunststukken', command= lambda: raise_frame(nietgeleend)).grid(row=3, column=3)
+Label(houder, text='').grid(row=4, column=2)
+Button(houder, text= 'Overzicht geleende kunststukken', command= lambda: raise_frame(geleend)).grid(row=5, column=3)
+Label(houder, text='').grid(row=6, column=2)
+Button(houder, text= 'Overzicht van bezoekers',command= lambda: raise_frame(bezoekers)).grid(row=7, column=3)
+Label(houder, text='').grid(row=8, column=2)
+Button(houder, text= 'Ticketnummer controle', command= lambda: raise_frame(controle)).grid(row=9, column=3)
+Label(houder, text='').grid(row=10, column=2, pady=10)
+Button(houder, text= 'Terug', command= lambda: raise_frame(main)).grid(row= 11, column=3)
 
 
 #nietgeleend
-
+Label(nietgeleend, text='Overzicht niet geleende kunststukken:').pack(pady=20)
 scrollbar = Scrollbar(nietgeleend)
 scrollbar.pack(side=RIGHT, fill=Y)
 
@@ -100,10 +112,12 @@ for line in range(101):                                         #hier moet lengt
 mylist.pack(expand=1, fill=BOTH)
 scrollbar.config( command = mylist.yview )
 
-Button(nietgeleend, text= 'Terug', command= lambda: raise_frame(houder)).pack(pady=10)
+Button(nietgeleend, text= 'Lenen').pack(pady=5)
+Button(nietgeleend, text= 'Terug', command= lambda: raise_frame(houder)).pack(pady=5)
+
 
 #geleend
-
+Label(geleend, text='Overzicht geleende kunststukken:').pack(pady=20)
 scrollbar = Scrollbar(geleend)
 scrollbar.pack(side=RIGHT, fill=Y)
 
@@ -118,7 +132,7 @@ Button(geleend, text= 'Terug', command= lambda: raise_frame(houder)).pack(pady=1
 
 
 #bezoekers
-
+Label(bezoekers, text='Overzicht van bezoekers:').pack(pady=20)
 scrollbar = Scrollbar(bezoekers)
 scrollbar.pack(side=RIGHT, fill=Y)
 
@@ -134,21 +148,19 @@ scrollbar.config(command=listbox.yview)
 Button(bezoekers, text= 'Terug', command= lambda: raise_frame(houder)).pack(pady=10)
 
 #controle
-Label(controle, text= '').grid(row=1, column=0)
-Label(controle, text='Voer hier de code in:').grid(row=2, column=0)
+Label(controle, text= '').grid(row=1, column=0, pady=20)
+Label(controle, text='Voer hier de code in:').grid(row=2, column=3, padx=125, pady=10)
 e1 = Entry(controle)
-e1.grid(row=2, column=3)
-Label(controle, text= '').grid(row=3, column=0)
-Button(controle, text='Check').grid(row=4, column=3)
+e1.grid(row=3, column=3)
+Label(controle, text= '').grid(row=4, column=0)
+Button(controle, text='Check').grid(row=5, column=3)
 
-Label(controle, text= '').grid(row=5, column=0)
-Button(controle, text= 'Terug', command= lambda: raise_frame(houder)).grid(row=6, column=3)
+Label(controle, text= '').grid(row=6, column=0)
+Button(controle, text= 'Terug', command= lambda: raise_frame(houder)).grid(row=7, column=3)
 
 
 root.title('')
-root.geometry('640x360')
+root.geometry('360x360')
 raise_frame(main)
 root.mainloop()
 
-#photo=PhotoImage(file)
-#label=Label(root, image=photo)
