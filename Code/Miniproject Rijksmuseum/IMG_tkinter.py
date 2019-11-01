24aec826063a9e6d8ae3f06d70ed223765e07fe9
Douwe de Jong <douwe.nicolaas@gmail.com>
2019-11-01 15:20:00 +0100

    added comments voor later

--- Code/Miniproject Rijksmuseum/IMG_tkinter.py
+++ Code/Miniproject Rijksmuseum/IMG_tkinter.py
@@ -5,18 +5,24 @@ from urllib.request import urlopen
 import Rijks_API_laad as api
 
 root = tk.Tk()
-
-url = str(api.imgKunstwerk(input("Naam van het kunstwerk: ")))
+# url is zoekfunctie via RijksAPI(de nachtwacht, vervangen)
+url = str(api.imgKunstwerk("de nachtwacht"))
 image_bytes = urlopen(url).read()
 data_stream = io.BytesIO(image_bytes)
 pil_image = Image.open(data_stream)
-pil_image = pil_image.resize((500, 500), Image.BILINEAR)
-fname = url.split('/')[-1]
+pil_image = pil_image.resize((350, 300), Image.BILINEAR)
+fname = "Gallerij"
 sf = "{}".format(fname)
 root.title(sf)
 tk_image = ImageTk.PhotoImage(pil_image)
+# link naar galerijhouder
+textLabel = tk.Label(root, text= str("NAAM\n"
+                                     "ADRES 123\n"
+                                     "PLAATS, 1234AB"), padx=10, pady=10)
+textLabel.pack()
+
 
 label = tk.Label(root, image=tk_image, bg='white')
 label.pack(padx=5, pady=5)
-root.geometry("500x500")
+root.geometry("360x360")
 root.mainloop()
